26a4a8596e557544c3644226f9a7a4116305143a
Neil Middleton <neil@heroku.com>
2013-07-09 19:33:45 +0100

    Add nodetime

--- web.js
+++ web.js
@@ -21,6 +21,13 @@ var elasticSearchClient = new ElasticSearchClient(serverOptions);
 var _index = "sample";
 var _type = 'document';
 
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: 'My Application Name' // optional
+  });
+}
+
 // Configuration
 
 app.configure(function () {
